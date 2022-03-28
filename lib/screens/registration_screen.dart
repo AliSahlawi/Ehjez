f5a253b7f3f6e6765a1b0f3458452abbc367921e
unknown <20180936@stu.uob.edu.bh>
2022-03-28 20:39:25 +0300

    added additional registration fields now we are missing DOB only

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:ehjez/constants.dart';
 import 'package:ehjez/components/rounded_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = 'registration_screen';
+
   const RegistrationScreen({Key? key}) : super(key: key);
 
   @override
@@ -12,12 +14,14 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-
+  final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
-
-  String email= "";
-  String password= "";
-
+  String email = "";
+  String password = "";
+  String name = "";
+  String carNumber = "";
+  String phoneNUmber = "";
+  late DateTime DOB = DateTime(1980,1,1);
 
   @override
   Widget build(BuildContext context) {
@@ -30,19 +34,18 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Row(
-              mainAxisAlignment:  MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: const [
                 Text(
                   'Register ',
                   style: TextStyle(
                       fontSize: 45.0,
                       fontWeight: FontWeight.w900,
-                      color: kTextColor
-                  ),
+                      color: kTextColor),
                 ),
               ],
-            )
-            ,SizedBox(
+            ),
+            SizedBox(
               height: 48.0,
             ),
             TextField(
@@ -67,20 +70,57 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   hintText: 'Enter your password.'),
             ),
             SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                name = value;
+              },
+              decoration:
+                  kTextFieldDecoration.copyWith(hintText: 'Enter your Name.'),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                carNumber = value;
+              },
+              decoration:
+              kTextFieldDecoration.copyWith(hintText: 'Enter your Car Number.'),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                phoneNUmber = value;
+              },
+              decoration:
+              kTextFieldDecoration.copyWith(hintText: 'Enter your Phone Number.'),
+            ),
+            SizedBox(
               height: 24.0,
             ),
             RoundedButton(
                 text: 'Register',
                 onPress: () async {
-
                   try {
                     final newUser = await _auth.createUserWithEmailAndPassword(
                         email: email, password: password);
-                    if (newUser!= null) {
+                    if (newUser != null) {
                       //Navigator.pushNamed(context, AnyScreen.id);
-                      print(newUser.user?.uid);
-                    }
+                      _firestore.collection('User').doc(newUser.user?.uid).set({
+                        'Name': name,
+                        'Email': email,
+                        'PhoneNum': phoneNUmber,
+                        'PlateNum':carNumber,
+                      });
 
+                    }
                   } catch (e) {
                     print(e);
                   }
